30a6ab9dc2b1056d71176be7d923ac455f08e73e
JacintaRoberts <jacinta.roberts@connect.qut.edu.au>
2020-03-27 19:35:59 +1000

    Set up project directories

---
+++ src/test/java/server/ServerTest.java
@@ -0,0 +1,22 @@
+package server;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+    /* Test 0: Declaring Server object
+     */
+    Server server;
+
+    /* Test 1: Constructing a Server object
+     */
+    @BeforeEach @Test
+    public void setUpServer() {
+        server = new Server();
+    }
+
+    /* Test 3: More tests...
+     */
+}
