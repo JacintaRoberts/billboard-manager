0a3fd79134d38a0eeb2de4eb0ff436db0f4407a2
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-04-04 19:40:07 +1000

    Finished UserControl
    Updated and finished Usercontrol

--- src/test/java/controlPanel/UserControlTest.java
+++ src/test/java/controlPanel/UserControlTest.java
@@ -22,58 +22,173 @@ class UserControlTest {
 //    }
 
 
-    /* Test x: Request to server to
-     * Description:
-     * Expected Output:
+    /* Test 2: Request to server to Create New Users (Success)
+     * Description: New method to create new users to the system. This will take a valid sesionToken, a unique Username
+     *              and a password string to create a new user
+     * Expected Output: A success feedback from the server
      */
-    public void listUsers(String sessionToken){
+//    public void createUserRequestTest(){
+//        userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//        assertTrue(serverResponse,"User Created!");
+//    }
 
-    }
 
-
-    /* Test x: Request to server to
-     * Description:
-     * Expected Output:
+    /* Test 3: Request to server to Create New Users (Fail)
+     * Description: New method to create new users to the system. This will take a valid sesionToken, a unique Username
+     *              and a password string to create a new user. This test will test if the username provided is not
+     *              unique
+     * Expected Output: An error from server
      */
-    public void createUserRequest(String currentUser, String newUsername, String sessionToken) {
+//    public void createUserRequestTest(){
+//        userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//        userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//        assertTrue(serverResponse,"Username Already Taken");
+//    }
 
-    }
 
-
-    /* Test x: Request to server to
-     * Description:
-     * Expected Output:
+    /* Test 4: Request to server to Create New Users (Fail)
+     * Description: New method to create new users to the system. This will take a valid sesionToken, a unique Username
+     *              and a password string to create a new user
+     * Expected Output: An error from server
      */
-    public void setUserPasswordRequest(String sessionToken, String newPassword) {
+//    public void createUserRequestTest(){
+//        userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//        assertTrue(serverResponse,"Insuffice User Permission");
+//    }
 
-    }
 
-
-    /* Test x: Request to server to
-     * Description:
-     * Expected Output:
+    /* Test 5: Request to server to list Current Users
+     * Description: Method to request to server to send a list of active users in the database. Requires a valid
+     *              sessionToken.
+     * Expected Output: A list of users
      */
-    public void createUsers(String sessionToken, String newUsername, String newPassword){
+//    public void listUsersTest(){
+//        userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//        userControl.createUserRequest("sessionToken", "NewUser2", "Pass2");
+//        List<String> testUserList = new ArrayList<String>();
+//        testUserList.add("NewUser1");
+//        testUserList.add("NewUser2");
+//        UserList userList = userControl.listUsers("sessionToken");
+//        assertArrayEquals(testUserList ,userList);
+//    }
 
-    }
 
-    /* Test x: Request to server to
-     * Description:
-     * Expected Output:
+    /* Test 6: Request to server to change password (Success)
+     * Description: Method to request to server to change a specific users password. Assumes a valid sessionToken is
+     *              running, and that user has permission. This test tests for themself.
+     * Expected Output: Success response from the server
      */
-    public void getUserPermission(String sessionToken, String currentUser){
+//    public void setUserPasswordRequestTest() {
+//        userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//        userControl.createUserRequest("sessionToken", "NewUser2", "Pass2");
+//        userControl.setUserPasswordRequest("sessionToken", "CAB302", "NewPassword");
+//        assertTrue(serverResponse, "Password Change Successfully");
+//    }
 
-    }
 
-
-    /* Test x: Request to server to
-     * Description:
-     * Expected Output:
+    /* Test 7: Request to server to change password (Success)
+     * Description: Method to request to server to change a specific users password. Assumes a valid sessionToken is
+     *              running, and that user has permission. This test tests for otherUsers.
+     * Expected Output: Success response from the server
      */
-    public void setUserPermission(String sessionToken, String currentUser, String targetUser, boolean[] permissions) {
+//    public void setUserPasswordRequestTest() {
+//        userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//        userControl.createUserRequest("sessionToken", "NewUser2", "Pass2");
+//        userControl.setUserPasswordRequest("sessionToken", "NewUser2", "NewPassword");
+//        assertTrue(serverResponse, "Password Change Successfully");
+//    }
 
-    }
 
+    /* Test 8: Request to server to change password (Fail)
+     * Description: Method to request to server to change a specific users password. Assumes a valid sessionToken is
+     *              running, and that user has permission. This test tests for insuffice permission
+     * Expected Output: Success response from the server
+     */
+//    public void setUserPasswordRequestTest() {
+//        userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//        userControl.createUserRequest("sessionToken", "NewUser2", "Pass2");
+//        userControl.setUserPasswordRequest("sessionToken", "NewUser2", "NewPassword");
+//        if (serverResponse == "No Permission"){
+//          throw new UserValueException();
+//        }
+//        }
+
+
+    /* Test 9: Request to server to change password (Fail)
+     * Description: Method to request to server to change a specific users password. Assumes a valid sessionToken is
+     *              running, and that user has permission. This test tests for nonexistent Users.
+     * Expected Output: Success response from the server
+     */
+//    public void setUserPasswordRequestTest() {
+//        userControl.setUserPasswordRequest("sessionToken", "NewUser3", "NewPassword");
+//        if (serverResponse == "No such User"){
+//          throw new NoUserValueException();
+//        }
+//    }
+
+
+    /* Test 10: Request to server to get User Permission from a user (Success)
+     * Description: Method to request userpermissions.
+     * Expected Output: Return of UserPermission in an array
+     */
+//    public void getUserPermissionTest(){
+//      userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//      int[] basicUserPermissionsExpected = {0,0,0,0};
+//      int[] basicUserPermissions = userControl.getUserPermission("sessionToken", "User1");;
+//      assertTrue(basicUserPermissionsExpected, basicUserPermissions);
+//    }
+
+
+    /* Test 11: Request to server to get User Permission from a user (Fail)
+     * Description: Get other User's Permissions from db - throw exception due to insufficient calling permissions
+     * Require "EditUsers" permission which is the 4th element in UserPermissions object
+     * e.g. [1,1,1,0] can't call the method.
+     * Expected Output: The permissions of requested user cannot be retrieved, throw InsufficientPermissionsException
+     * TODO: Implement Fake db through HashMap to verify
+     */
+//    @Test
+//    public void getOtherUserPermissionsException() {
+//      userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//      assertThrows(InsufficientPermissionsException.class, () -> {
+//          userControl.getPermissions("root", sessionToken);
+//       });
+//    }
+
+
+    /* Test 12: Request to server to set user permissions of a user (success)
+     * Description: Method to set a user permission for another person
+     * Expected Output: Success Message
+     */
+//    public void setUserPermissionTest() {
+//      userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//      userControl.setUserPermission("sessionToken", "NewUser1", boolean[] {1,1,1,1})
+//      assertTrue(serverResponse, "Permission Changed");
+//    }
+
+    
+    /* Test 12: Request to server to set user permissions of a user (Fail)
+     * Description: Method to set a user permission for another person. Such Person does not exists
+     * Expected Output: Exception error
+     */
+//    public void setUserPermissionTest() {
+//      userControl.setUserPermission("sessionToken", "NewUser1", boolean[] {1,1,1,1})
+//      if (serverResponse == "No Such User"){
+//          throw new NoUserValueException();
+//      }
+//    }
+
+
+    /* Test 12: Request to server to set user permissions of a user (Fail)
+     * Description: Method to set a user permission for another person. No permission to do so.
+     * Expected Output: Exception error
+     */
+//    public void setUserPermissionTest() {
+//      userControl.createUserRequest("sessionToken", "NewUser1", "Pass1");
+//      userControl.setUserPermission("sessionToken", "NewUser1", boolean[] {1,1,1,1})
+//      if (serverResponse == "No Permission"){
+//        throw new UserValueException();
+//      }
+//    }
 
 
 }
