bdd02368b7280079488970cacfc493ec36b2cb56
JacintaRoberts <jacinta.roberts@connect.qut.edu.au>
2020-04-11 01:53:12 +1000

    Started Schedule Admin
    Started the unit tests for schedule admin (server unit testing) and separated out declaration of Mock DB as it's own test.

--- src/test/java/server/BillboardAdminTest.java
+++ src/test/java/server/BillboardAdminTest.java
@@ -12,7 +12,6 @@ class BillboardAdminTest {
      * Expected Output: BillboardAdmin object is declared
      */
     BillboardAdmin billboardAdmin;
-    DatabaseMock<String, ArrayList<String>> billboardTableMock;
 
     /* Test 1: Constructing a BillboardAdmin object
      * Description: BillboardAdmin Object should be able to be created to handle requests from control panel
@@ -24,13 +23,19 @@ class BillboardAdminTest {
         billboardAdmin = new BillboardAdmin();
     }
 
-    /* Test 2: Constructing a DatabaseMock object for Billboard Table
+    /* Test 2: Declaring Database Mock object
+     * Description: Database Mock simulates Maria DB for testing purposes.
+     * Expected Output: DatabaseMock object for Billboard Table is declared
+     */
+    DatabaseMock<String, ArrayList<String>> billboardTableMock;
+
+    /* Test 3: Constructing a DatabaseMock object for Billboard Table
      * Description: DatabaseMock should be used to verify unit tests and setup of the DB
      * Expected Output: DatabaseMock object is instantiated to mimic Billboard Table
      */
     @BeforeEach
     @Test
-    public void setUpUserTableMock() {
+    public void setUpBillboardTableMock() {
         billboardTableMock = new DatabaseMock<>();
         // Add a dummy value (insert more as necessary or create separate function to populate)
         billboardTableMock.addValue("myBillboard", new ArrayList<String>( Arrays.asList("Creator",
@@ -39,36 +44,35 @@ class BillboardAdminTest {
     }
 
 
-    /* Test 3: Create Billboard in Billboard Table (Success)
-     * Description: Receive create billboard request from Control Panel, will have billboard name, user, and xml.
+    /* Test 4: Create Billboard in Billboard Table (Success)
+     * Description: Receive create billboard request from CP, will have sessionToken, billboard name, and xml.
      *              Assume sessionToken is valid.
      * Expected Output: Billboard is added to the table and returns "Pass: Billboard Created"
      */
 //    @Test
 //    public void createABillboardRequest(){
-//        String serverResponse = billboardAdmin.createBillboardRequest("Billboard1", "userFromSessionToken", xmlCode);
+//        String serverResponse = billboardAdmin.createBillboardRequest("sessionToken", "Billboard1", xmlCode);
 //        assertEquals(serverResponse, "Pass: Billboard Created");
 //    }
 
 
-    /* Test 4: Create Billboard - Duplicate Billboard Name (Exception Handling)
-     * Description: Receive create billboard request from Control Panel, will have billboard name, user, and xml.
+    /* Test 5: Create Billboard - Duplicate Billboard Name (Exception Handling)
+     * Description: Receive create billboard request from CP, will have sessionToken, billboard name, and xml.
      *              Expect Fail due to Invalid Name (same name). Assume sessionToken is valid.
      * Expected Output: Billboard is not added to the table and returns "Fail: Billboard Name Already Exists"
      * // TODO: Investigate if there is a way to check for a more specific JDBC SQL error
      */
 //    @Test
 //    public void createSameNameBillboardRequest(){
-//        String serverResponse = billboardAdmin.createBillboardRequest("Billboard1", "userFromSessionToken", xmlCode);
+//        String serverResponse = billboardAdmin.createBillboardRequest("sessionToken", "Billboard1", xmlCode);
 //        assertEquals(serverResponse, "Fail: Billboard Name Already Exists");
 //        // Assert that the DB throws an SQL Exception for duplicate key
 //        assertThrows(SQLException);
 //    }
 
 
-
-    /* Test 5: Create Billboard - Illegal Characters in Billboard Name (Exception Handling)
-     * Description: Receive create billboard request from Control Panel, will have billboard name, user, and xml.
+    /* Test 6: Create Billboard - Illegal Characters in Billboard Name (Exception Handling)
+     * Description: Receive create billboard request from CP, will have sessionToken, billboard name, and xml.
      *          Expect Fail due to Invalid Name (Illegal Characters). Assume sessionToken is valid.
      * Expected Output: Billboard is not added to the table and returns "Fail: Billboard Name Already Exists"
      * // TODO: WE SHOULD PROB TELL THE USER ON THE GUI NOT TO ENTER WEIRD CHARS BUT JUST CHECK ANYWAY IN CASE THEY
@@ -76,26 +80,26 @@ class BillboardAdminTest {
      */
 //    @Test
 //    public void createIllegalNameBillboardRequest(){
-//        String serverResponse = billboardAdmin.createBillboardRequest("!@#$%^&*()~", "userFromSessionToken", xmlCode);
+//        String serverResponse = billboardAdmin.createBillboardRequest("sessionToken", "!@#$%^&*()~", xmlCode);
 //        assertEquals(serverResponse, "Fail: Billboard Contains Illegal Character");
 //        // Have to create this exception ourselves - if (name.contains("!@#$%^&*()~)) throw Exception ->
 //        assertThrows(IllegalBillboardNameException);
 //    }
 
 
-    /* Test 6: Edit Billboard in Billboard Table (Success)
+    /* Test 7: Edit Billboard in Billboard Table (Success)
      * Description: Receive edit billboard request from Control Panel, need to have suffice user permission to edit
      *              board and user needs to provide a new xmlCode. Assume sessionToken is valid.
      * Expected Output: Billboard is edited in the table and returns "Pass: Billboard Edited"
      */
 //    @Test
 //    public void editABillboardRequest(){
-//        String serverResponse = billboardAdmin.editBillboardRequest("Billboard1", "userFromSessionToken", xmlCode);
+//        String serverResponse = billboardAdmin.editBillboardRequest("sessionToken", "Billboard1", xmlCode);
 //        assertEquals(serverResponse, "Pass: Billboard Edited");
 //    }
 
 
-    /* Test 7: Edit Billboard - Insufficient User Permission (Exception Handling)
+    /* Test 8: Edit Billboard - Insufficient User Permission (Exception Handling)
      * Description: Receive edit billboard request from Control Panel, need to have suffice user permissions to edit
      *              board and user needs to provide a new xmlCode. Assume sessionToken is valid.
      *              Fail due to insufficient permission.
@@ -103,12 +107,12 @@ class BillboardAdminTest {
      */
 //    @Test
 //    public void editABillboardRequestNoPermission(){
-//        String serverResponse = billboardAdmin.editBillboardRequest("Billboard1", "userFromSessionToken", xmlCode);
+//        String serverResponse = billboardAdmin.editBillboardRequest("sessionToken", "Billboard1", xmlCode);
 //        assertEquals(serverResponse, "Fail: Insufficient User Permission");
 //    }
 
 
-    /* Test 8: Edit Billboard - Billboard Name Does Not Exist (Exception Handling)
+    /* Test 9: Edit Billboard - Billboard Name Does Not Exist (Exception Handling)
      * Description: Receive edit billboard request from Control Panel, need to have suffice user permissions to edit
      *              board and user needs to provide a new xmlCode. Assume sessionToken is valid.
      *              Fail due to non-existent board (does not exists or was deleted before clicking button).
@@ -117,26 +121,26 @@ class BillboardAdminTest {
      */
 //    @Test
 //    public void editABillboardRequestNoBillboard(){
-//        String serverResponse = billboardAdmin.editBillboard("Billboard1", "userFromSessionToken", xmlCode);
+//        String serverResponse = billboardAdmin.editBillboard("sessionToken", "Billboard1", xmlCode);
 //        assertEquals(serverResponse, "Fail: Billboard Does not Exist");
 //        // Billboard Name does not exist in DB
 //        assertThrows(SQLException);
 //    }
 
 
-    /* Test 9: Delete Billboard in Billboard Table (Success)
+    /* Test 10: Delete Billboard in Billboard Table (Success)
      * Description: Receive delete billboard request from CP. Assume sessionToken is valid, and target exists.
      * Expected Output: Billboard is deleted in the table and returns "Pass: Billboard Deleted"
      */
 //    @Test
 //    public void deleteABillboard(){
-//        String serverResponse = billboardAdmin.deleteBillboard("sampleSessionToken", "Billboard1");
-//        // sampleSessionToken required to be passed in to check permissions inside function
+//        String serverResponse = billboardAdmin.deleteBillboard("sessionToken", "Billboard1");
+//        // sessionToken required to be passed in to check permissions inside function
 //        assertEquals(serverResponse, "Pass: Billboard Deleted");
 //    }
 
 
-    /* Test 10: Delete Billboard - Billboard Name Does Not Exist (Exception Handling)
+    /* Test 11: Delete Billboard - Billboard Name Does Not Exist (Exception Handling)
      * Description: Receive delete billboard request from CP. Assume sessionToken is valid, and target exists.
      *              Fail due to non-existent billboard name requested.
      * Expected Output: Billboard is not deleted in the table and returns "Fail: Billboard Does not Exist"
@@ -144,26 +148,26 @@ class BillboardAdminTest {
      */
 //    @Test
 //    public void deleteABillboardNoBillboard(){
-//        String serverResponse = billboardAdmin.deleteBillboard("sampleSessionToken","Billboard1");
+//        String serverResponse = billboardAdmin.deleteBillboard("sessionToken","Billboard1");
 //        assertEquals(serverResponse, "Fail: Billboard Does Not Exist");
 //        // Billboard Name does not exist in DB
 //        assertThrows(SQLException);
 //    }
 
 
-    /* Test 11: Delete Billboard - Insufficient Permissions (Exception Handling)
+    /* Test 12: Delete Billboard - Insufficient Permissions (Exception Handling)
      * Description: Receive delete billboard request from CP. Assume sessionToken is valid, and target exists.
      *              Fail due to insufficient permissions.
      * Expected Output: Billboard is not deleted in the table and returns "Fail: Insufficient User Permission"
      */
 //    @Test
 //    public void deleteABillboardNoPermission(){
-//        String serverResponse = billboardAdmin.deleteBillboard("sampleSessionToken","Billboard1");
+//        String serverResponse = billboardAdmin.deleteBillboard("sessionToken","Billboard1");
 //        assertEquals(serverResponse, "Fail: Insufficient User Permission");
 //    }
 
 
-    /* Test 12: List Billboards from Billboard Table (Success)
+    /* Test 13: List Billboards from Billboard Table (Success)
      * Description: Receive list billboard request from CP. Assume sessionToken is valid. Returns an array
      *              list of billboards.
      * Expected Output: Returns billboard names array and "Pass: Billboard List Returned"
@@ -181,7 +185,7 @@ class BillboardAdminTest {
 //    }
 
 
-    /* Test 13: List Billboards - Billboard Name Does Not Exist (Exception Handling)
+    /* Test 14: List Billboards - Billboard Name Does Not Exist (Exception Handling)
      * Description: Receive list billboard request from CP. Assume sessionToken is valid.
      *              Fails because there is no billboard to return (empty).
      * Expected Output: Returns billboard names array and "Fail: No Billboard Exists"
@@ -198,7 +202,7 @@ class BillboardAdminTest {
 //    }
 
 
-    /* Test 14: Billboard Information from Billboard Table (Success)
+    /* Test 15: Billboard Information from Billboard Table (Success)
      * Description: Receive billboard information request from CP. Information provided will include
      *              information such as billboardName, Creator, xmlCode.
      * Expected Output: Returns Billboard Information object and "Pass: Billboard Info Returned"
@@ -216,7 +220,7 @@ class BillboardAdminTest {
 //    }
 
 
-    /* Test 15: Billboard Information - Billboard Name Does Not Exist (Exception Handling)
+    /* Test 16: Billboard Information - Billboard Name Does Not Exist (Exception Handling)
      * Description: Receive billboard information request from CP. Information provided will include
      *              information such as billboardName, Creator, xmlCode.
      *              The method will fail because the billboard does not exist.
@@ -233,7 +237,7 @@ class BillboardAdminTest {
 //    }
 
 
-    /* Test 16: Billboard Information - Insufficient Permissions (Exception Handling)
+    /* Test 17: Billboard Information - Insufficient Permissions (Exception Handling)
      * Description: Receive billboard information request from CP. Information provided will include
      *              information such as billboardName, Creator, xmlCode.
      *              The method will fail because no permission error.
@@ -244,9 +248,6 @@ class BillboardAdminTest {
 //        BillboardInformation serverResponse = billboardAdmin.getBillboardInformation("sessionToken","Billboard500");
 //        assertEquals(billboardInformation.getServerResponse(), "Fail: Insufficient User Permission");
 //    }
-
-
-
 
 
 }
