3580e38bed8e01d663983ae47cdf9892de599e3c
KanuAgarwal <KanuAgarwal@users.noreply.github.com>
2020-04-10 16:14:43 +1000

    Updated mock database
    Loaded xml files into mock database and accessed message, picture and information tags

--- src/main/java/viewer/Viewer.java
+++ src/main/java/viewer/Viewer.java
@@ -2,18 +2,20 @@ package viewer;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 public class Viewer {
 
     // Create and set up the JFrame to show the viewer
-    private JFrame viewer = new JFrame ("Billboard Viewer");
+    private static JFrame viewer = new JFrame ("Billboard Viewer");
 
     /**
      * Exits the window if the user presses the escape key.
      */
-    public void listenEscapeKey() {
+    public static void listenEscapeKey() {
         KeyListener escListener = new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent event) {
@@ -29,7 +31,7 @@ public class Viewer {
     /**
      * Exits the window if the user clicks anywhere on the window.
      */
-    public void listenMouseClick() {
+    public static void listenMouseClick() {
         MouseListener clickListener = new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent event) {
@@ -44,7 +46,9 @@ public class Viewer {
      * Formats the display window using the mock database depending on whether there is a message,
      * picture, information or a combination.
      */
-    public void formatBillboard() {
+    public static void formatBillboard(String message, String picture, String information) {
+
+
 
     }
 
@@ -52,7 +56,7 @@ public class Viewer {
     /**
      * If there are no billboards to display, display a message for the user.
      */
-    public void noBillboardDisplay() {
+    public static void noBillboardDisplay() {
         // Create a label to display a message and format it
         JLabel message = new JLabel("There are no billboards to display right now.", SwingConstants.CENTER);
         message.setFont(new Font(message.getFont().getName(), Font.PLAIN, 50));
@@ -62,7 +66,7 @@ public class Viewer {
     /**
      * Show the GUI for displaying the billboard.
      */
-    public void showViewer() {
+    public static void showViewer() {
 
         // Displaying the window to be completely full screen
         viewer.setExtendedState(Frame.MAXIMIZED_BOTH);
@@ -71,13 +75,20 @@ public class Viewer {
         viewer.setVisible(true);
     }
 
+    /**
+     * Displays an example billboard
+     */
+    public static void displayBillboard() {
+        ArrayList<String> billboardTags = MockBillboardDatabase.getBillboard(1);
+        noBillboardDisplay();
+        listenEscapeKey();
+        listenMouseClick();
+        showViewer();
+    }
+
 
     public static void main(String[] args ) {
-        Viewer BillboardDemo = new Viewer();
-        BillboardDemo.noBillboardDisplay();
-        BillboardDemo.listenEscapeKey();
-        BillboardDemo.listenMouseClick();
-        BillboardDemo.showViewer();
+        displayBillboard();
 
     }
 
