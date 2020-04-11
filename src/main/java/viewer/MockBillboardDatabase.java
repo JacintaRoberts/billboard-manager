7879a224465bdbceca2bbf1485c23fb3d475ad22
KanuAgarwal <KanuAgarwal@users.noreply.github.com>
2020-04-11 13:57:06 +1000

    Updated mock database and billboard viewer
    Moved a function from mockdatabase to viewer as this is where it belongs, and added functionality to viewer

--- src/main/java/viewer/MockBillboardDatabase.java
+++ src/main/java/viewer/MockBillboardDatabase.java
@@ -1,13 +1,6 @@
 package viewer;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /*
@@ -17,99 +10,24 @@ actual database.
 
 public class MockBillboardDatabase {
 
-    // Initialise ArrayLists to story the message, picture and information tags from the XML
-    private static ArrayList<String> billboardMessages = new ArrayList<>();
-    private static ArrayList<String> billboardPictures = new ArrayList<>();
-    private static ArrayList<String> billboardInformation = new ArrayList<>();
-    private static int num_files = 16;
+    // Initialise ArrayList to store the XML files in
+    private static ArrayList<File> xmlFiles = new ArrayList<>();
+    private static int numFiles = 16;
 
     /**
-     * Import xml billboard file and set up the mock database
+     * Import xml billboard files and store it an ArrayList - this will act as the mock database.
+     * TODO: Change the pathname so it's not on local device
      */
-    public static void setupDatabase() {
+    public static ArrayList<File> setupDatabase() {
 
-        // Try to parse the xml file, if it doesn't work throw an exception
-        for (int i = 0; i < num_files; i++) {
-
-            try {
-                // Load xml file
-                File xmlFile = new File("/Users/kanu1/Documents/GitHub/CAB302-Billboard/src/main/billboards/"
-                        + (i+1) + ".xml");
-
-                // Use DocumentBuilderFactory to parse xml file
-                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-                Document xmlDoc = dBuilder.parse(xmlFile);
-
-                // Create an element which is the billboard tag
-                NodeList billboardNodeList = xmlDoc.getElementsByTagName("billboard");
-                Node billboardNode = billboardNodeList.item(0);
-                Element billboardElement = (Element) billboardNode;
-
-                // If the message tag exists, add the message content to the ArrayList, if not add an empty string
-                if (billboardElement.getElementsByTagName("message").getLength() == 1) {
-                    String message = billboardElement.getElementsByTagName("message").item(0).getTextContent();
-                    billboardMessages.add(i, message);
-                } else {
-                    billboardMessages.add(i, "");
-                }
-
-                // If the picture tag exists, add the url attribute to the ArrayList, if not add an empty string
-                if (billboardElement.getElementsByTagName("picture").getLength() == 1) {
-                    Node pictureNode = billboardElement.getElementsByTagName("picture").item(0);
-                    Element pictureElement = (Element) pictureNode;
-                    String picture = pictureElement.getAttribute("url");
-                    billboardPictures.add(i, picture);
-                } else {
-                    billboardPictures.add(i, "");
-                }
-
-                // If the information tag exists, add the information content to the ArrayList, if not add an empty string
-                if (billboardElement.getElementsByTagName("information").getLength() == 1){
-                    String information = billboardElement.getElementsByTagName("information").item(0).getTextContent();
-                    billboardInformation.add(i, information);
-                } else {
-                    billboardInformation.add(i, "");
-                }
-
-                // Print the tags to the console
-//                System.out.println("Billboard " + (i+1));
-//                System.out.println("Message: " + billboardMessages.get(i));
-//                System.out.println("Picture: " + billboardPictures.get(i));
-//                System.out.println("Information: " + billboardInformation.get(i) + "\n");
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        // Import each xml file and add it to the ArrayList
+        for (int i = 0; i < numFiles; i++) {
+            File xmlFile = new File("/Users/kanu1/Documents/GitHub/CAB302-Billboard/src/main/billboards/"
+                    + (i+1) + ".xml");
+            xmlFiles.add(i, xmlFile);
         }
 
+        return xmlFiles;
     }
-
-    /**
-     * Gets an ArrayList of the message, picture and information associated with a certain billboard
-     * @param billboardNo - the billboard number we want the tags of
-     * @return billboardTags - an ArrayList containing the content in the tags of the billboard
-     */
-    public static ArrayList<String> getBillboard(int billboardNo) {
-        // Set up the mock database
-        setupDatabase();
-
-        // Create an ArrayList to store the tag content in
-        ArrayList<String> billboardTags = new ArrayList<>();
-
-        // Add the content from the tags to the ArrayList
-        billboardTags.add(0, billboardMessages.get(billboardNo));
-        billboardTags.add(1, billboardPictures.get(billboardNo));
-        billboardTags.add(2, billboardInformation.get(billboardNo));
-
-        return billboardTags;
-    }
-
-
-    // Main method
-//    public static void main(String[] args) {
-//        MockBillboardDatabase dbTest = new MockBillboardDatabase();
-//        setupDatabase();
-//    }
 
 }
--- src/main/java/viewer/Viewer.java
+++ src/main/java/viewer/Viewer.java
@@ -1,16 +1,219 @@
 package viewer;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import javax.imageio.ImageIO;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
 
 public class Viewer {
 
     // Create and set up the JFrame to show the viewer
     private static JFrame viewer = new JFrame ("Billboard Viewer");
+
+
+    /**
+     * Extracts the xml file that we want to display.
+     * TODO: This method should be the only one which needs to be changed when the actual database is connected
+     */
+    public static File extractXMLFile(int fileNum) {
+        ArrayList<File> xmlFiles = MockBillboardDatabase.setupDatabase();
+        return xmlFiles.get(fileNum-1);
+    }
+
+
+    /**
+     * Extracts data from an xml file for creating the billboard - i.e. it gets the content from the
+     * message, picture and information tags as well as the custom colours
+     * @param xmlFile - an xml file from which to extract information from
+     * @return billboardTags - an ArrayList which stores the background colour, message, message colour, picture,
+     *      information, and information colour of the billboard, if there is no content for one or more of these tags,
+     *      it stores an empty string
+     * TODO: Change from an ArrayList to a TreeMap? - this will need to change below as well
+     */
+    public static ArrayList<String> extractDataFromXML(File xmlFile) {
+        // Initiate an ArrayList to return
+        ArrayList<String> billboardData = new ArrayList<>();
+
+        // Try parsing the xml file - if it fails catch the exception
+        try {
+            // Use DocumentBuilderFactory to parse xml file
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document xmlDoc = dBuilder.parse(xmlFile);
+
+            // Create an element which is the billboard tag
+            NodeList billboardNodeList = xmlDoc.getElementsByTagName("billboard");
+            Node billboardNode = billboardNodeList.item(0);
+            Element billboardElement = (Element) billboardNode;
+
+            // Add the background colour
+            String backgroundColour = billboardElement.getAttribute("background");
+            billboardData.add(0, backgroundColour);
+
+            // If the message tag exists, add the message and colour, else add an empty string
+            if (billboardElement.getElementsByTagName("message").getLength() == 1) {
+                Node messageNode = billboardElement.getElementsByTagName("message").item(0);
+                Element messageElement = (Element) messageNode;
+                String message = messageElement.getTextContent();
+                String messageColour = messageElement.getAttribute("colour");
+                billboardData.add(1, message);
+                billboardData.add(2, messageColour);
+            } else {
+                billboardData.add(1, "");
+                billboardData.add(2, "");
+            }
+
+            // If the picture tag exists, add the url or data attribute, else add an empty string
+            if (billboardElement.getElementsByTagName("picture").getLength() == 1) {
+                Node pictureNode = billboardElement.getElementsByTagName("picture").item(0);
+                Element pictureElement = (Element) pictureNode;
+                String picture = "";
+                if (pictureElement.getAttribute("url") != "") {
+                    picture = pictureElement.getAttribute("url");
+                } else {
+                    picture = pictureElement.getAttribute("data");
+                }
+                billboardData.add(3, picture);
+            } else {
+                billboardData.add(3, "");
+            }
+
+            // If the information tag exists, add the information and colour, else add an empty string
+            if (billboardElement.getElementsByTagName("information").getLength() == 1) {
+                Node informationNode = billboardElement.getElementsByTagName("information").item(0);
+                Element informationElement = (Element) informationNode;
+                String information = informationElement.getTextContent();
+                String informationColour = informationElement.getAttribute("colour");
+                billboardData.add(4, information);
+                billboardData.add(5, informationColour);
+            } else {
+                billboardData.add(4, "");
+                billboardData.add(5, "");
+            }
+
+            // Print the tags to the console - for testing
+//            System.out.println("Background Colour: " + billboardData.get(0));
+//            System.out.println("Message: " + billboardData.get(1));
+//            System.out.println("Message Colour: " + billboardData.get(2));
+//            System.out.println("Picture: " + billboardData.get(3));
+//            System.out.println("Information: " + billboardData.get(4));
+//            System.out.println("Information Colour: " + billboardData.get(5));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return billboardData;
+    }
+
+
+    /**
+     * If there are no billboards to display, display a message for the user.
+     */
+    public static void noBillboardToDisplay() {
+        // Create a label to display a message and format it
+        JLabel messageLabel = new JLabel("There are no billboards to display right now.", SwingConstants.CENTER);
+        messageLabel.setFont(new Font(messageLabel.getFont().getName(), Font.PLAIN, 50));
+        viewer.add(messageLabel);
+    }
+
+
+    /**
+     * Displays a billboard which has only a message
+     * TODO: Set the colour attribute to the JLabel
+     */
+    public static void MessageOnlyBillboard(ArrayList<String> billboardData) {
+        // Extract the message from the billboard data
+        String message = billboardData.get(1);
+        String messageColour = billboardData.get(2);
+
+        // Create a label to display a message and format it
+        JLabel messageLabel = new JLabel(message, SwingConstants.CENTER);
+        messageLabel.setFont(new Font(messageLabel.getFont().getName(), Font.PLAIN, 50));
+        if (messageColour != "") {
+            messageLabel.setForeground(Color.decode(messageColour));
+            messageLabel.setBackground(Color.decode(messageColour));
+        }
+        viewer.add(messageLabel);
+    }
+
+
+    /**
+     * Displays a billboard which has only a picture
+     * TODO
+     */
+    public static void PictureOnlyBillboard(ArrayList<String> billboardData) {
+
+    }
+
+
+    /**
+     * Displays a billboard which has only a information
+     * TODO
+     */
+    public static void InformationOnlyBillboard(ArrayList<String> billboardData) {
+
+    }
+
+
+    /**
+     * Displays a billboard which has a message and picture
+     * TODO
+     */
+    public static void MessagePictureBillboard(ArrayList<String> billboardData) {
+
+    }
+
+
+    /**
+     * Displays a billboard which has a message and information
+     * TODO
+     */
+    public static void MessageInformationBillboard(ArrayList<String> billboardData) {
+
+    }
+
+    /**
+     * Displays a billboard which has a picture and information
+     * TODO
+     */
+    public static void PictureInformationBillboard(ArrayList<String> billboardData) {
+
+    }
+
+
+    /**
+     * Displays a billboard which has all the features: a message, picture and information
+     * TODO
+     */
+    public static void AllFeaturesBillboard(ArrayList<String> billboardData) {
+
+    }
+
+
+    /**
+     * Formats the display window using the mock database depending on whether there is a message,
+     * picture, information or a combination.
+     * TODO: Insert a switch statement which chooses between different cases - calls on different functions
+     *      i.e. there is a function for each case displaying the billboard properly.
+     * TODO: This function will deal with the background colour as this might be relevant to all billboards.
+     */
+    public static void formatBillboard(ArrayList<String> billboardData) {
+
+
+    }
+
 
     /**
      * Exits the window if the user presses the escape key.
@@ -28,6 +231,7 @@ public class Viewer {
         viewer.addKeyListener(escListener);
     }
 
+
     /**
      * Exits the window if the user clicks anywhere on the window.
      */
@@ -42,26 +246,6 @@ public class Viewer {
         viewer.addMouseListener(clickListener);
     }
 
-    /**
-     * Formats the display window using the mock database depending on whether there is a message,
-     * picture, information or a combination.
-     */
-    public static void formatBillboard(String message, String picture, String information) {
-
-
-
-    }
-
-
-    /**
-     * If there are no billboards to display, display a message for the user.
-     */
-    public static void noBillboardDisplay() {
-        // Create a label to display a message and format it
-        JLabel message = new JLabel("There are no billboards to display right now.", SwingConstants.CENTER);
-        message.setFont(new Font(message.getFont().getName(), Font.PLAIN, 50));
-        viewer.add(message);
-    }
 
     /**
      * Show the GUI for displaying the billboard.
@@ -75,12 +259,18 @@ public class Viewer {
         viewer.setVisible(true);
     }
 
+
     /**
-     * Displays an example billboard
+     * Displays the billboards
+     * TODO: Update this to actually display a billboard
+     * TODO: Get this talk to the server to see the schedule
      */
     public static void displayBillboard() {
-        ArrayList<String> billboardTags = MockBillboardDatabase.getBillboard(1);
-        noBillboardDisplay();
+        File fileToDisplay = extractXMLFile(11);
+        ArrayList<String> billboardTags = extractDataFromXML(fileToDisplay);
+        // MessageOnlyBillboard(billboardTags);
+        // formatBillboard(billboardTags);
+        noBillboardToDisplay();
         listenEscapeKey();
         listenMouseClick();
         showViewer();
