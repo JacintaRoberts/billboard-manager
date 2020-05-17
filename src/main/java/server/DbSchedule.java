5c0ead94b833e4d36d7fc512ff9573a6bc13584b
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-05-17 22:33:33 +1000

    Update to SQL for Scheduling

--- src/main/java/server/DbSchedule.java
+++ src/main/java/server/DbSchedule.java
@@ -3,11 +3,34 @@ package server;
 public class DbSchedule {
 
     // Set Fields for DbSchedule
-    private String StartDateTime;
     private String BillboardName;
+    private String StartTime;
     private String Duration;
+    private String CreationDateTime;
+    private String Repeat;
+    private String Sunday;
+    private String Monday;
+    private String Tuesday;
+    private String Wednesday;
+    private String Thursday;
+    private String Friday;
+    private String Saturday;
 
-    public DbSchedule(String StartDateTime, String BillboardName, String Duration) {
+    // Constructor for DbSchedule
+    public DbSchedule(String BillboardName, String Duration, String CreationDateTime, String Repeat,
+                      String Sunday, String Monday, String Tuesday,
+                      String Wednesday, String Thursday, String Friday, String Saturday) {
+        this.BillboardName = BillboardName;
+        this.Duration = Duration;
+        this.CreationDateTime;
+        this.Repeat;
+        this.Sunday;
+        this.Monday;
+        this.Tuesday;
+        this.Wednesday;
+        this.Thursday;
+        this.Friday;
+        this.Saturday;
     }
 
 
--- src/main/java/server/ScheduleAdmin.java
+++ src/main/java/server/ScheduleAdmin.java
@@ -1,4 +1,79 @@
 package server;
 
+import java.io.IOException;
+import java.sql.*;
+
 public class ScheduleAdmin {
+
+    // Custom SQL Strings for Specific Queries
+    public static final String COUNT_SCHEDULE_SQL = "SELECT COUNT(*) FROM Schedules";
+    public static final String DROP_SCHEDULE_TABLE = "DROP TABLE IF EXISTS `BillboardDatabase`.`Schedules`";
+    public static final String CREATE_SCHEDULE_TABLE = "CREATE TABLE IF NOT EXISTS `BillboardDatabase`.`Schedules` (\n" +
+            "      `BillboardName` varchar(255) NOT NULL default '',\n" +
+            "      `StartTime` TIME NOT NULL default CURRENT_TIMESTAMP,\n" +
+            "      `Duration` INT NOT NULL default 1,\n" +
+            "      `CreationDateTime` DATETIME NOT NULL default CURRENT_TIMESTAMP,\n" +
+            "      `Repeat` INT default NULL,\n" +
+            "      `Sunday` bool NOT NULL default 0,\n" +
+            "      `Monday` bool NOT NULL default 0,\n" +
+            "      `Tuesday` bool NOT NULL default 0,\n" +
+            "      `Wednesday` bool NOT NULL default 0,\n" +
+            "      `Thursday` bool NOT NULL default 0,\n" +
+            "      `Friday` bool NOT NULL default 0,\n" +
+            "      `Saturday` bool NOT NULL default 0,\n" +
+            "      PRIMARY KEY (`BillboardName`)";
+
+    // Custom Parameters for connection
+    private static Connection connection;
+
+
+
+    /**
+     * Stores Database Queries: Schedules. This is a generic method which Make sures Schedules is made with default data.
+     * <p>
+     * This method always returns immediately.
+     * @return
+     */
+    public String createScheduleTable() throws IOException, SQLException {
+        String resultMessage;
+
+        connection = DbConnection.getInstance();
+        Statement countSchedule = connection.createStatement();
+        ResultSet rs = null;
+        try {
+            rs = countSchedule.executeQuery(COUNT_SCHEDULE_SQL);
+            rs.next();
+            String count = rs.getString(1);
+            if (count.equals("0")){
+//                createGenericBillboard();
+                resultMessage = "Data Filled";
+            } else {
+                resultMessage = "Data Exists";
+            }
+        } catch (SQLSyntaxErrorException throwables) {
+            rs = countSchedule.executeQuery(CREATE_SCHEDULE_TABLE);
+//            createGenericBillboard();
+            resultMessage = "Table Created";
+        }
+        return resultMessage;
+    }
+
+
+    /**
+     * Delete Table: Schedules. This is a generic method which deletes the table.
+     * <p>
+     * This method always returns immediately.
+     * @return
+     */
+    public String dropScheduleTable() throws IOException, SQLException{
+        String resultMessage;
+        connection = DbConnection.getInstance();
+        Statement dropSchedule = connection.createStatement();
+        ResultSet rs = dropSchedule.executeQuery(DROP_SCHEDULE_TABLE);
+        resultMessage = "Schedule Table Dropped";
+        return resultMessage;
+    }
+
+
+
 }
