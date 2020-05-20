2db1f52995e860c25a7518fbd1eca0c44b84ae3c
PCotter-dev <n9763872@qut.edu.au>
2020-05-20 11:45:38 +1000

    Revert "fixed controller and bbcreateview"
    This reverts commit 7c112bec0e915493c1e77eeb8e80fba5aebbb286.

--- src/main/java/controlPanel/BBCreateView.java
+++ src/main/java/controlPanel/BBCreateView.java
@@ -78,6 +78,8 @@ public class BBCreateView extends AbstractGenericView
     void createComponents()
     {
         createDrawingPanel();
+//        createDrawingToolbar();
+//        createBBOptionsMenu();
     }
 
     /**
