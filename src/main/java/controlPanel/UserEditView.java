beb2d31e13f53d1c91893bf53b875f013b441251
PCotter-dev <n9763872@qut.edu.au>
2020-05-24 14:38:31 +1000

    set password to null if nothing selected

--- src/main/java/controlPanel/UserEditView.java
+++ src/main/java/controlPanel/UserEditView.java
@@ -90,6 +90,10 @@ public class UserEditView extends AbstractUserView
                 passwordText = password;
             }
         }
+        else
+        {
+            passwordText = null;
+        }
     }
 
     protected void showErrorMessage()
